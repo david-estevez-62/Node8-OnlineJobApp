6e08850974549af01395e79719f7499c2136b3ee
Eric Hruska <e.hruska@aol.com>
2015-02-25 17:28:48 -0700

    Node8-OnlineJobAppForm

--- models/applicants.js
+++ models/applicants.js
@@ -6,6 +6,17 @@ var applicantSchema = mongoose.Schema({
 
 var Applicant = mongoose.model('Applicant', applicantSchema);
 
+
+var applicant =  new Applicant({
+	name: req.body.name,
+	bio: req.body.bio,
+	skills: req.body.skills,
+	years: req.body.years,
+	why: req.body.why
+})
+
+applicant.save()
+
 // var movieInstance = new Movie({
 // 	title: 'Star Wars'
 // });
